aa9e3642e866e20442921980b8ac7f901d9bdbf1
qu-een-li <qu_een_li@icloud.com>
2025-04-24 16:48:59 +0300

    добавление апи в форме регистрации и на главной страничке, а также менюшка слева и заголовок с временем теперь присутствуют везде

--- registrationform.py
+++ registrationform.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, IntegerField, SelectField
 from wtforms.validators import DataRequired
 
 
@@ -9,7 +9,9 @@ class RegistrationForm(FlaskForm):
     birthday = StringField('Дата Рождения Ребёнка', validators=[DataRequired()])
     pasport_number = IntegerField('Номер Паспорта', validators=[DataRequired()])
     pasport_series = IntegerField('Серия Паспорта', validators=[DataRequired()])
-    school = StringField('Школа, местоположение школы', validators=[DataRequired()])
+    region = SelectField('Регион', choices=[], validators=[DataRequired()])
+    city = SelectField('Город', choices=[], validators=[DataRequired()])
+    school = SelectField('Школа', choices=[], validators=[DataRequired()])
     PFDO = IntegerField('ПФДО', validators=[DataRequired()])
     parent_phone = StringField('Телефон Родителя', validators=[DataRequired()])
     student_phone = StringField('Телефон Ученика', validators=[DataRequired()])
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import random
 import datetime
+import requests
 
 from flask import Flask, render_template, request, redirect, make_response, session
 from config import KEY_CSRF
@@ -8,11 +9,16 @@ from data import db_session
 from data.students import Student
 from data.ways import Way
 from flask_login import LoginManager, login_user, logout_user
+from flask import jsonify
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = KEY_CSRF
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+SERVICE_KEY = "b23f635fb23f635fb23f635f26b110e9c3bb23fb23f635fda3f614c627d359f88d1a62e"
+API_VERSION = '5.199'
+REGION_ID = 1086244
 
 
 @login_manager.user_loader
@@ -50,6 +56,62 @@ def post():
         session.commit()
         return redirect("/post")
     return render_template("registration.html", form=form)
+
+
+@app.route('/api/regions', methods=['GET'])
+def get_regions():
+    regions = [{
+        "id": REGION_ID,
+        "title": "Республика Северная Осетия — Алания"
+    }]
+    return jsonify(regions)
+
+
+@app.route('/api/cities/<int:region_id>', methods=['GET'])
+def get_cities(region_id):
+    url = 'https://api.vk.com/method/database.getCities'
+    params = {
+        'access_token': SERVICE_KEY,
+        'v': API_VERSION,
+        'region_id': region_id,
+        'need_all': 1,
+        'lang': 'ru',
+        'count': 1000,
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        if 'response' in data and 'items' in data['response']:
+            cities = [{'id': city['id'], 'title': city['title']} for city in data['response']['items']]
+            return jsonify(cities)
+        return jsonify([])
+    except Exception as e:
+        print(f"Ошибка при выборе городов: {e}")
+        return jsonify([])
+
+
+@app.route('/api/schools/<int:city_id>', methods=['GET'])
+def get_schools(city_id):
+    url = 'https://api.vk.com/method/database.getSchools'
+    params = {
+        'access_token': SERVICE_KEY,
+        'v': API_VERSION,
+        'city_id': city_id,
+        'count': 1000,
+        'lang': 'ru'
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        if 'response' in data and 'items' in data['response']:
+            schools = [{'id': school['id'], 'title': school['title']} for school in data['response']['items']]
+            return jsonify(schools)
+        return jsonify([])
+    except Exception as e:
+        print(f"Ошибка при выборе школ: {e}")
+        return jsonify([])
 
 
 if __name__ == '__main__':
